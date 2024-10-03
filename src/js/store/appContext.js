ef41623bc641113e42baf5f7fa324711dd1ad77e
Alejandro Dayekh García <maestrodayekh@gmail.com>
2024-10-03 19:50:04 +0000

    Casi casi

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -27,7 +27,7 @@ const injectContext = PassedComponent => {
 			 **/
 			state.actions.getAgenda();
 		}, []);
-
+			
 		// The initial value for the context is not null anymore, but the current state of this component,
 		// the context will now have a getStore, getActions and setStore functions available, because they were declared
 		// on the state of this component
--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -1,5 +1,6 @@
-import React, { useContext,useState } from "react";
+import React, { useContext,useState, useEffect } from "react";
 import { Context } from "../store/appContext";
+import {Modal, Button, Form} from "react-bootstrap"
 
 const Contacts = () => {
     const {store , actions} = useContext(Context);
@@ -10,24 +11,18 @@ const Contacts = () => {
     const [address, setAddress] = useState("")
     const [currentId, setCurrentId] = useState(null)
     
-    const inputName = (event) => {
-        setName({...name, [event.target.name]: event.target.value})
-    } 
+    useEffect (() => {actions.getAgenda()}, [actions])
 
-    const inputPhone = (event) => {
-        setPhone({...phone, [event.target.phone]: event.target.value})
-    }
+    const inputName = (event) => setName(event.target.value);
+    
+    const inputPhone = (event) => setPhone(event.target.value);
 
-    const inputEmail = (event) => {
-        setEmail({...email, [event.target.email]: event.target.value})
-    } 
+    const inputEmail = (event) => setEmail(event.target.value);
 
-    const inputAddress = (event) => {
-        setAddress({...address, [event.target.address]: event.target.value})
+    const inputAddress = (event) => setAddress(event.target.value);
 
-    } 
 
-    const memoryEdit = (contact) => {
+    const openModal = (contact) => {
         setName(contact.name)
         setPhone(contact.phone)
         setEmail(contact.email)
@@ -36,6 +31,13 @@ const Contacts = () => {
         setShowModal(true); 
     };
 
+    const closeModal = () => setShowModal(false)
+
+    const handleUpdate = () => {
+       const updateContact = {name, phone, email, address};
+      actions.updateContact(currentId)
+      console.log (...updateContact, currentId);
+    }
     return (
         <div>
             {store.contacts.map((contact, index) => {
@@ -50,53 +52,44 @@ const Contacts = () => {
                             </div>
                             <button onClick={() => actions.deleteContact(contact.id)} className="btn btn-danger">Delete</button>
                         
-                          <button type="button" onClick={() =>memoryEdit(contact)} className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                          <button type="button" onClick={() => openModal(contact)} className="btn btn-primary">
                                Edit Contact
                           </button>
-
 
                        </div>
                     </div>
                 )
             })}
-                {showModal && ( 
-                      <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                             <div className="modal-dialog" role="document">
-                                 <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                                        <button type="button" onClick={() => setShowModal(false)} className="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div className="modal-body">
-                                    <form>
-                                        <div className="form-group mt-3 mb-3">
-                                            <label htmlFor="formGroupName">Name</label>
-                                            <input type="text" name="name" className="form-control" id="formGroupName"  placeholder="Name" onChange={inputName}></input>
-                                        </div>
-                                        <div className="form-group mt-3 mb-3">
-                                            <label htmlFor="formGroupPhone">Phone</label>
-                                            <input type="text" name="phone" className="form-control" id="formGroupPhone" placeholder="Phone" onChange={inputPhone}></input>
-                                        </div>
-                                        <div className="form-group mt-3 mb-3">
-                                            <label htmlFor="formGroupEmail">Email</label>
-                                            <input type="text" name="email" className="form-control" id="formGroupEmail" placeholder="Email" onChange={inputEmail}></input>
-                                        </div>
-                                        <div className="form-group mt-3 mb-3">
-                                            <label htmlFor="formGroupAddress">Address</label>
-                                            <input type="text" name="address" className="form-control" id="formGroupAddress" placeholder="Address" onChange={inputAddress}></input>
-                                        </div>
-                                    </form>
-                                    </div>
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => setShowModal(false)}>Close</button>
-                                        <button type="button" className="btn btn-primary">Save changes</button>
-                                    </div>
-                                 </div>
-                            </div>
-                        </div>
-                )}
+                <Modal show= {showModal} onHide={closeModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Contact</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body> 
+                        <Form>
+                                <Form.Group className="mb-3" controlId="formGroupName">
+                                <Form.Label name="name"></Form.Label>
+                                <Form.Control type="text" placeholder="Name" value={name} onChange={inputName} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formGroupPhone">
+                                <Form.Label name="phone"></Form.Label>
+                                <Form.Control type="text" placeholder="Phone" value={phone} onChange={inputPhone} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formGroupEmail">
+                                <Form.Label name="email"></Form.Label>
+                                <Form.Control type="text" placeholder="Email" value={email} onChange={inputEmail} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formGroupAddress">
+                                <Form.Label name="address"></Form.Label>
+                                <Form.Control type="text" placeholder="Address" value={address} onChange={inputAddress} />
+                                </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={closeModal}>Cancel</Button>
+                        <Button variant="primary" onClick={handleUpdate}>Save Changes</Button>
+                    </Modal.Footer>
+                  </Modal>
+        
                      <a href="https://silver-zebra-wr9vgv76xvg6cgwrj-3000.app.github.dev/contacts/addcontacts">
                     <button className="btn btn-primary mt-3">Add new contact</button>
                     </a> 
